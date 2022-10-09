e2ec88e322856b43061253b5f517b07f9c5f9c50
TenaciousTapioca <89610605+TenaciousTapioca@users.noreply.github.com>
2022-10-09 16:29:10 -0400

    Updating current implementation

--- roadElement.js
+++ roadElement.js
@@ -1,55 +1,120 @@
-// define what points of a grid are T, B, R, C
+// define what points of a grid are T, B, R
 class coordinate {
-    constructor(x, y, element) {
-      this.x = x;
-      this.y = y;
-      this.elem = element;
+  constructor(x, y, element) {
+    this.x = x;
+    this.y = y;
+    this.elem = element;
+    this.neighbors = [];
+    
+    // at least one must be false (cars can't go backwards on roads here)
+    this.direction = {'up': false, 'left': false, 'right': false, 'down': false};
+  }
+  
+  // fetch a single neighbor in the given direction (given as 'way')
+  seeNeighbor(way) {
+    switch (way) {
+      case "up":
+        return this.neighbors[0];
+      case 'left':
+        return this.neighbors[1];
+      case 'right':
+        return this.neighbors[2];
+      case 'down':
+        return this.neighbors[3];
+      default:
+        return -1;
     }
-}
-
-// each road is stored as a single-linked list; all are stored in some container for checking
-class road {
-  constructor(point) {
-    this.x = point.x;
-    this.y = point.y;
-    this.next = null;
   }
 }
 
-class linkedRoad {
-  constructor() {
-    this.beginning = null;
-    this.ending = null;
-    this.size = 0;
-  }
+// We could implement the car class to inherit its tile's coordinate class.
+// This way, the cars can operate on top of the roads without breaking road's properties
+// class car {}
+
+// compare x and y values of both tiles
+function areEqual(first, second) {
+  if ((first.x === second.x) && (first.y === second.y)) { return true;  }
+  else                                                  { return false; }
 }
 
-//function insertBefore(ptr, aRoad) {
-//  let temp = new road(aRoad);
-//  if ()
-//}
-
-// **will greatly change in the future**
-// identify if new road placement is legal (must grow road from an edge)
-function isRoadLegal(point) {
-  occupied = 0              // number of sides connected by another road
-  // check above
-  if ((point.y != 0) && (grid[point.x][point.y - 1].elem == "R")) { ++occupied; }
-  // check left
-  if ((point.x != 0) && (grid[point.x - 1][point.y].elem == "R")) { ++occupied; }
-  // check right
-  if ((point.x != (cols - 1)) && (grid[point.x + 1][point.y].elem == "R")){ ++occupied; }
-  // check below
-  if ((point.y != (rows - 1)) && (grid[point.x][point.y + 1].elem == "R")) { ++occupied; }
-    
-  // check if point is at the edge of grid
-  isEdge = false;
+// check if point is at the edge of the grid
+function isEdge(point) {
+  let result = false;
   if ((point.y == 0) || (point.y == (rows - 1)) || (point.x == 0) || (point.x == (cols - 1))) {
-    isEdge = true;
+    result = true;
   }
+  return result;
+}
+
+// Obtain an array of a tile's 4 nearest tiles in this order [up, left, right, down]
+// if the tile is at the edge, there is 1 outlier; if it's a corner, 2 outliers
+function getNeighbors(point) {
+  let result = [];
+  let outlier = new coordinate(-1, -1, 'B');
+  
+  // check above
+  if (point.y != 0) { result.push(grid[point.x][point.y - 1]); }
+  else              { result.push(outlier); }
+  // check left
+  if (point.x != 0) { result.push(grid[point.x - 1][point.y]); }
+  else              { result.push(outlier); }
+  // check right
+  if (point.x != (cols - 1)) { result.push(grid[point.x + 1][point.y]); }
+  else                       { result.push(outlier); }
+  // check down
+  if (point.y != (rows - 1)) { result.push(grid[point.x][point.y + 1]); }
+  else                       { result.push(outlier); }
     
-  // if not roadLegal, do a subtle screen shake (future)
-  if (occupied >= 2) { return false; }
-  else if ((occupied == 0) && (!isEdge)) { return false; }
-  else { return true; }
+  return result;
+}
+
+// given the initial and a current road, assign the road's connectivity as needed
+function assignDirection(initial, current) {
+  let index = initial.neighbors.findIndex(i => i.x === current.x && i.y === current.y);
+  if (index != -1) {
+    // find opposite side of given index (E.g., 0->Up is opposite of 3->Down)
+    let opposite;
+    opposite = getOpposite(index);
+    
+    // assign directions as necessary
+    if ((initial.direction[Object.keys(initial.direction)[index]] != true) &&
+        (current.direction[Object.keys(current.direction)[opposite]] != true)) {
+      initial.direction[Object.keys(initial.direction)[index]] = true;
+    } else {
+      initial.direction[Object.keys(initial.direction)[index]] = false;
+    }
+  }
+}
+
+// remove a road by simply right-clicking it (can't drag & delete multiple roads)
+function removeRoad(point) {
+  if (point.elem != 'R') { return; }
+  for (let i = 0; i < 4; ++i) {            // if intermediate road, don't delete
+    if (point.direction[Object.keys(point.direction)[i]] === true) {
+      return;
+    }
+  } 
+
+  // for any road that goes into this tile, make their respective direction flag false
+  for (let i = 0; i < 4; ++i) {
+    let nearby = point.seeNeighbor(Object.keys(point.direction)[i]);
+    if (nearby.direction[Object.keys(nearby.direction)[getOpposite(i)]] === true) {
+      nearby.direction[Object.keys(nearby.direction)[getOpposite(i)]] = false;
+    }
+  }
+  point.elem = 'B';
+  point.neighbors = [];
+}
+
+//               [0 -> Up] 
+// [1 -> Left]   [ Point ]   [2 -> Right]
+//              [3 -> Down]
+// Given an index, return the integer on other side of [ Point ]
+function getOpposite(index) {
+  let opposite;
+  if (index === 0)        { opposite = 3; } 
+  else if (index === 3)   { opposite = 0; }
+  else if (index === 1)   { opposite = 2; }
+  else                    { opposite = 1; }
+  return opposite;
 }
--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@ let divisor = 20;
 let cols;
 let rows;
 let grid;
+let prev = new coordinate(-1, -1, 'B');
 
 // setup the grid for traffic to take place
 function setup() {
-  createCanvas(720, 400);    // width = 720; height = 400
+  const canvas = createCanvas(720, 400);    // width = 720; height = 400
+  // disable right-click menu pop-up when right-clicking on canvas
+  canvas.elt.addEventListener("contextmenu", (e) => e.preventDefault());
   
   // calculate number of columns and rows for grid
   cols = floor(width / divisor);
@@ -25,34 +28,85 @@ function setup() {
   }
 }
 
-// (0,0) is top left of the grid
+// (0,0) is top left of the grid; this function is continuously called
 function draw() {
   background(255);
   
   // color the grid accordingly
   for (let i = 0; i < cols; ++i) {
     for (let j = 0; j < rows; ++j) {
-      fill(175);                      // color per pixel
+      fill(175);                                  // color per tile
       if (grid[i][j].elem == "R") { fill(255); } 
-      stroke(0);                      // border color of pixels
-      rect(i * divisor, j * divisor, divisor - 1, divisor - 1);
+      stroke('lightgray');                      // border color of pixels
+      strokeWeight(2);
+      rect(i * divisor, j * divisor, divisor - 1, divisor - 1); // 20 x 20 squares
+      
+      // will be replaced with a function that places the appropriate sprites
+      // atm, it places blue points on roads to show the direction of traffic
+      addPoints(grid[i][j]);
     }
   }
 }
 
-// currently changes a pixel to R or B
+// record initial 'R'-tile, then go straight to mouseDragged()
+// if it's a right-click, delete a non-intermediate road if possible
 function mousePressed() {
-  spot = grid[floor(mouseX / divisor)][floor(mouseY / divisor)];
-  
-  // Print is for debugging to see grid coordinates upon clicking
-  //print("(" + floor(mouseX / divisor) + ", " + floor(mouseY / divisor) + ")" + " Element is " + spot.elem);
-  
-  if ((spot.elem != "R") && isRoadLegal(spot)) {
-    spot.elem = "R";
-  } else {
-    spot.elem = "B";
-  }
-  redraw()
+  if (!isInsideCanvas()) { return; }
+  prev = grid[floor(mouseX / divisor)][floor(mouseY / divisor)];
+  if (mouseButton === RIGHT) {
+    removeRoad(grid[floor(mouseX / divisor)][floor(mouseY / divisor)]);
+  } 
 }
 
+// change a 'B'-tile into a new road
+// to continue building the same road, click & hold on an existing road tile and drag
+// it horizontally and/or vertically to grow it
+function mouseDragged() {
+  if (!isInsideCanvas()) { return; }
+  let spot = grid[floor(mouseX / divisor)][floor(mouseY / divisor)];
+  if (mouseButton === LEFT) {
+    
+    // create a new road if a 'B'-tile is clicked; otherwise, grow an existing road
+    if ((spot.elem != 'R') && (areEqual(prev, spot))) {
+      spot.elem = 'R';
+      spot.neighbors = getNeighbors(spot);
+    } else if (!areEqual(prev, spot) && (prev.neighbors.findIndex(i => i.x === spot.x && i.y === spot.y) != -1)) {
+      spot.elem = 'R';
+      spot.neighbors = getNeighbors(spot);
+      
+      // assign direction according to the newly added route
+      assignDirection(prev, spot);
+      prev = spot;
+    
+    }
+  }
+}
 
+// do not take any inputs residing outside of the canvas
+function isInsideCanvas() {
+  let x = mouseX, y = mouseY;
+  if (mouseX < 0 || mouseX > (width - 1) || mouseY < 0 || mouseY > (height - 1)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// will be replaced with road sprites
+// atm, blue points signifies which tiles the a tile can go into
+function addPoints(spot) {
+  p0 = spot.x * divisor;
+  p1 = spot.y * divisor;
+  up = spot.direction[Object.keys(spot.direction)[0]];
+  left = spot.direction[Object.keys(spot.direction)[1]];
+  right = spot.direction[Object.keys(spot.direction)[2]];
+  down = spot.direction[Object.keys(spot.direction)[3]];
+  
+  stroke('cyan');
+  strokeWeight(6);
+  if (up) { point(p0 + 10, p1 + 3); }
+  if (left) { point(p0 + 3, p1 + 10); }
+  if (right) { point(p0 + 17, p1 + 10); }
+  if (down) { point(p0 + 10, p1 + 17); }
+  strokeWeight(0);
+}
