7beaf8d034e5c2a97e00757f3a74844303746fa5
SamiAljailani <samialjailani1@gmail.com>
2022-10-16 21:14:48 -0400

    removed console.logs

--- car.js
+++ car.js
@@ -43,11 +43,9 @@ class Car{
 function moveCar(grid, map, i,j){
     let car = map[i][j];//car is a reference
     let nextCoordinate =car.coordinate.seeNeighbor(car.dir);
-    console.log(car.dir);
     //only move a car when the next tile is free and the neighbor is not an outlier 
     if(nextCoordinate != -1 && nextCoordinate.elem != 'B' && map[nextCoordinate.x][nextCoordinate.y] == undefined){
         if(grid[nextCoordinate.x][nextCoordinate.y].elem === 'T' && grid[nextCoordinate.x][nextCoordinate.y].currentInput != getOppositeDirection(car.dir)){
-            
             return;
         }
         car.setCoordinate(nextCoordinate);
